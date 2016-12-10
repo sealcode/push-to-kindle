d17ffee1bdac513cabaa110e308313d882f950d4
madjer22 <maciej.nowak.zslg@gmail.com>
2016-12-10 21:24:28 +0100

    Views linked and double press back added

--- app/src/main/java/org/sealcode/pushtokindle/MainActivity.java
+++ app/src/main/java/org/sealcode/pushtokindle/MainActivity.java
@@ -1,10 +1,21 @@
 package org.sealcode.pushtokindle;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText sender, receiver, subject;
+    TextInputLayout senderLayout, receiverLayout;
+    Button send;
+
+    String from, to, url, title, domain;
+    boolean doubleBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +24,28 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        sender = (EditText) findViewById(R.id.sender);
+        receiver = (EditText) findViewById(R.id.receiver);
+        subject = (EditText) findViewById(R.id.subject);
+        senderLayout = (TextInputLayout) findViewById(R.id.senderLayout);
+        receiverLayout = (TextInputLayout) findViewById(R.id.receiverLayout);
+        send = (Button) findViewById(R.id.send);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(doubleBack) {
+            super.onBackPressed();
+            return;
+        }
+        doubleBack = true;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBack = false;
+            }
+        }, 2000);
     }
 
 }
