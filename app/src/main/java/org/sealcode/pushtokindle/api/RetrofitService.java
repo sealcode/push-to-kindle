dcd6cbfcf38cb827923a3e562d0f1e8dfb92ca81
madjer22 <maciej.nowak.zslg@gmail.com>
2016-12-11 11:32:16 +0100

    Sending and checking article methods implemented

--- app/src/main/java/org/sealcode/pushtokindle/api/RetrofitService.java
+++ app/src/main/java/org/sealcode/pushtokindle/api/RetrofitService.java
@@ -1,12 +1,19 @@
 package org.sealcode.pushtokindle.api;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.widget.Button;
 import android.widget.Toast;
 
 import org.sealcode.pushtokindle.R;
 
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -38,6 +45,62 @@ public class RetrofitService {
         shared = Shared.getInstance(context);
         toast =  Toast.makeText(context, "", Toast.LENGTH_LONG);
         this.context = context;
+    }
+
+    public void sendArticle(String url, final String from, final String to, String title, String domain, String receiverId) {
+        Call call = service.sendWebsite(ACTION_SEND, url, from, title, receiverId, domain, SAVE_FILE);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                try {
+                    ResponseBody responseBody = (ResponseBody) response.body();
+                    String body = responseBody.string();
+                    CharSequence unable =  UNABLE_RETRIEVE;
+                    if(body.contains(unable)) showToast(R.string.cant_send);
+                    else {
+                        showToast(R.string.sent);
+                        shared.writeSender(from);
+                        shared.writeReceiver(to);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                enableButton();
+            }
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                if(t.getMessage().contains(UNABLE_RESOLVE)) showToast(R.string.turn_internet);
+                else showToast(context.getString(R.string.couldnt_send) + t.getMessage());
+                enableButton();
+            }
+        });
+    }
+
+    public void checkArticle(String url) {
+        final ProgressDialog progressDialog;
+        progressDialog = ProgressDialog.show(context, context.getString(R.string.checking), context.getString(R.string.progress), true);
+        Call call = service.checkConversion(ACTION_CHECK, url);
+        call.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                try {
+                    ResponseBody responseBody = (ResponseBody) response.body();
+                    String body = responseBody.string();
+                    CharSequence unable =  UNABLE_RETRIEVE;
+                    if(body.contains(unable)) showToast(R.string.cant_send);
+                    else enableButton();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                progressDialog.dismiss();
+            }
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                if(t.getMessage().contains(UNABLE_RESOLVE)) showToast(R.string.turn_internet);
+                else showToast(context.getString(R.string.couldnt_send) + t.getMessage());
+                progressDialog.dismiss();
+            }
+        });
     }
 
     public void showToast(int id) {
